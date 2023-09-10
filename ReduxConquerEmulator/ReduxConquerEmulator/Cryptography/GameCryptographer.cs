25a0dddaa7a6a158ab98317242170526452ee0fa
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2023-09-11 00:59:27 +0200

    Small change

--- ReduxConquerEmulator/ReduxConquerEmulator/Cryptography/GameCryptographer.cs
+++ ReduxConquerEmulator/ReduxConquerEmulator/Cryptography/GameCryptographer.cs
@@ -5,7 +5,7 @@ namespace Redux.Cryptography
 {
     public class GameCryptography
     {
-        OpenSSL.Blowfish _blowfish;
+        Blowfish _blowfish;
         public GameCryptography(byte[] key)
         {
             _blowfish = new Blowfish(BlowfishAlgorithm.CFB64);
@@ -15,13 +15,13 @@ namespace Redux.Cryptography
         public void Decrypt(byte[] packet)
         {
             byte[] buffer = _blowfish.Decrypt(packet);
-            System.Buffer.BlockCopy(buffer, 0, packet, 0, buffer.Length);
+            Buffer.BlockCopy(buffer, 0, packet, 0, buffer.Length);
         }
 
         public void Encrypt(byte[] packet)
         {
             byte[] buffer = _blowfish.Encrypt(packet);
-            System.Buffer.BlockCopy(buffer, 0, packet, 0, buffer.Length);
+            Buffer.BlockCopy(buffer, 0, packet, 0, buffer.Length);
         }
 
         public Blowfish Blowfish
